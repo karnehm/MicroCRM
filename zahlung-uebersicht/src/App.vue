8a7a95feec8bf359104385a6f9e7f7a130e0348e
Dominic Karnehm <dominic.karnehm@ars.de>
2018-11-29 16:02:29 +0100

    First version of <payment-view>

---
+++ zahlung-uebersicht/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <PaymentView v-on:bill="onBill" v-bind:do-update="updater" />
+    <button v-on:click="update">Update now</button>
+  </div>
+</template>
+
+<script>
+    import PaymentView from './components/Payment-View'
+    import { Subject } from "rxjs"
+
+    export default {
+        name: 'App',
+        components: {
+            PaymentView
+        },
+        methods: {
+            onBill: function (x) {
+                alert(x)
+            },
+            update: function () {
+              this.updater.next();
+            }
+        },
+        data() {
+            return {
+                updater: Subject
+            };
+        },
+        created() {
+            this.updater = new Subject();
+
+        }
+    }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ zahlung-uebersicht/src/components/Payment-View.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <h2>Zahlung Übersicht</h2>
+    <b-alert :show="isError" variant="danger">{{errorMsg}}</b-alert>
+    <b-table v-if="data" :items="data" :fields="fields" bordered>
+      <template slot="table-caption">
+        <div style="text-align: right; margin-right: 20px">
+          Gesamt: {{totalAmount}} EUR
+        </div>
+      </template>
+      <template slot="bill" slot-scope="data">
+        <a href="#" v-on:click="$emit('bill', data.item)" >
+          {{data.value}}
+        </a>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    import bTable from 'bootstrap-vue/es/components/table/table'
+    import bAlert from 'bootstrap-vue/es/components/alert/alert'
+    import { Subject } from "rxjs"
+
+    export default {
+        name: 'PaymentView',
+        model: {
+            props: ['doUpdate', 'customername'],
+            event: 'bill'
+        },
+        props: {
+            doUpdate: Subject,
+            customername: String
+        },
+        components: {
+            'b-table': bTable,
+            'b-alert': bAlert
+        },
+        data () {
+            return {
+                data: null,
+                totalAmount: 0,
+                lastUpdate: 0,
+                errorMsg: 'There was an error. Sorry',
+                isError: false,
+                url: 'http://localhost:3000/payment',
+                fields: [
+                    {
+                        key: 'amountdate',
+                        label: 'Rechnungsdatum',
+                        sortable: true
+                    }, {
+                        key: 'bill',
+                        label: 'Rechnungsnummer'
+                    }, {
+                        key: 'customer',
+                        label: 'Kunde',
+                        sortable: true
+                    }, {
+                        key: 'amount',
+                        label: 'Betrag',
+                        formatter: (x) => { return x + ' EUR' }
+                    }
+                ]
+            }
+        },
+        methods: {
+            updateValues: function (customerName) {
+                const url = customerName ? this.url + '?customer=' + customerName : this.url
+                axios
+                    .get(url)
+                    .then(response => {
+                        this.data = response.data
+                        this.isError = false
+                        this.totalAmount = 0;
+                        response.data.map((x) => { this.totalAmount += Number(x.amount) })
+                    })
+                    .catch(() => {
+                        this.data = null
+                        this.isError = true
+                    })
+            }
+        },
+        mounted () {
+            this.updateValues(this.customername)
+            if(this.doUpdate) {
+                this.doUpdate.subscribe(() => this.updateValues(this.customername));
+            }
+        },
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  @import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+  @import '../../node_modules/bootstrap-vue/dist/bootstrap-vue.css';
+  * {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  h1, h2 {
+    text-align: left;
+  }
+</style>
